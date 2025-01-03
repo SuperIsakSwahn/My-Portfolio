8d56eefb66340c8c146fc53da6c3d07d8fcb667d
SuperIsakSwahn <71665693+SuperIsakSwahn@users.noreply.github.com>
2025-01-03 15:47:40 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Function to handle download button click
+    function handleDownloadButtonClick(filePath, fileName) {
+        // Create a link element
+        const link = document.createElement('a');
+        link.href = filePath; // Path to the file
+        link.download = fileName; // Name of the file to be downloaded
+        document.body.appendChild(link);
+        link.click(); // Trigger the click event
+        document.body.removeChild(link); // Clean up
+    }
+
+    // Get the download buttons
+    const manhuntButton = document.getElementById('downloadManhunt');
+    const piglinButton = document.getElementById('downloadPiglinTrades');
+    const cakeButton = document.getElementById('downloadCakeRecept');
+
+    // Add event listeners to the buttons
+    manhuntButton.addEventListener('click', function() {
+        handleDownloadButtonClick('/downloads/ManhuntImproved.jar', 'ManhuntImproved.jar');
+    });
+
+    piglinButton.addEventListener('click', function() {
+        handleDownloadButtonClick('/downloads/PiglinPearlDrops.jar', 'PiglinPearlDrops.jar');
+    });
+    cakeButton.addEventListener("click", function () {
+        handleDownloadButtonClick('/downloads/Milkfree chocolate cake prescription.txt', 'Milkfree chocolate cake prescription.txt');
+    });
+
+    function toggleDarkMode() {
+        document.body.classList.toggle("dark-mode");
+        updateButtonColor();
+    }
+
+    function updateButtonColor() {
+        const isDarkMode = document.body.classList.contains('dark-mode');
+        const buttons = document.querySelectorAll('button');
+        const anchors = document.querySelectorAll('a')
+        anchors.forEach(a => {
+            a.style.color = isDarkMode ? "#fff" : "black";
+        })
+        buttons.forEach(button => {
+            button.style.color = isDarkMode ? '#fff' : '';
+        })
+    }
+    const darkModeButton = document.getElementById('darkMode')
+
+    darkModeButton.addEventListener('click',function () {
+        toggleDarkMode();
+    })
+    updateButtonColor();
+    toggleDarkMode();
+});
